8de186b8e18ea4dc34501ba60a8ffc5fcb8eabf5
DavideCotigliani <davide.cotigliani@gmail.com>
2025-03-19 16:23:40 +0100

    [UPD] Bonus, 3rd exercise done

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -19,6 +19,8 @@ const longNames = teachers.filter(teacher => teacher.length >= 5);
 console.log(longNames);
 
 // 3. Rimuovi 'Ed' dall'array teachers
+teachers.splice(5, 1)
+console.log(teachers);
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
